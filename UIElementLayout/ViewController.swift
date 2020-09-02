787227f540696d52e993c61cf4cdf4d8c5682ee6
AlyJ <alina.jarmolica2@gmail.com>
2020-09-02 18:59:46 +0300

    COMMIT

--- UIElementLayout/ViewController.swift
+++ UIElementLayout/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftButtonrUp: UIButton!
+    @IBOutlet weak var rightButtonUp: UIButton!
+    @IBOutlet weak var leftButtonDown: UIButton!
+    @IBOutlet weak var rightButtonDown: UIButton!
+    
+    @IBOutlet var styleOutletCollection: [UIButton]!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var mainViewImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        styleUI()
     }
+        func styleUI() {
+        mainViewImage.layer.cornerRadius = 10
+        mainViewImage.layer.borderWidth = 2
+        mainViewImage.layer.borderColor = UIColor.systemPink.cgColor
+        
+        myLabel.layer.cornerRadius = 10
+        myLabel.layer.borderWidth = 2
+        myLabel.layer.borderColor = UIColor.systemPink.cgColor
+        
+        styleOutletCollection.forEach { button  in
+            button.layer.cornerRadius = 10
+            button.layer.borderWidth = 2
+            button.layer.borderColor = UIColor.systemPink.cgColor
+        }
+        }
+    
+    }
+    
+func touchDragOutside(_ sender: Any) {
+        
+        print("touchdragoutside")
+    }
+    
 
-
-}
 
